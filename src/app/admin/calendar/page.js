9b9e812328a2423bdd1b29f978a9c77da8d53c16
tlthang281201 <thang281201@gmail.com>
2023-12-04 22:44:24 +0700

    first commit

---
+++ src/app/admin/calendar/page.js
@@ -0,0 +1,17 @@
+import Calendar from "../../../components/Calender";
+
+export const metadata = {
+  title: "Calendar Page | Next.js E-commerce Dashboard Template",
+  description: "This is Calendar page for TailAdmin Next.js",
+  // other metadata
+};
+
+const CalendarPage = () => {
+  return (
+    <>
+      <Calendar />
+    </>
+  );
+};
+
+export default CalendarPage;
